41f4f06a5a7d7c3367610b75a611b4dda98d5a72
Leonardo Eloy <eloy@hp.com>
2016-08-24 22:06:21 -0600

    First plugin integraton attempt

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('Loading Mutuca Pull Request');
+
+class MutucaPullRequest {
+  constructor() {}
+
+  pluginName() {
+    return "mutuca-pull-request";
+  }
+  
+  cardSize() {
+    return 4;
+  }
+
+  cardPath() {
+    return `${__dirname}/card.html`;
+  }
+}
+
+module.exports = MutucaPullRequest;
