a8d5cb38a838af52bd94a468322ef76a6282f3e2
Leonardo Eloy <eloy@hp.com>
2016-08-25 10:39:23 -0600

    Added toString()

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 console.log('Loading Mutuca Pull Request');
 
+const version = '1.0.2';
+
 class MutucaPullRequest {
   constructor() {}
 
   pluginName() {
     return "mutuca-pull-request";
   }
-  
+
   cardSize() {
     return 4;
   }
 
   cardPath() {
     return `${__dirname}/card.html`;
+  }
+
+  toString() {
+    return this.pluginName() + "@" + version;
   }
 }
 
