5579b09a63ca9f5092da70bb50eb55e110d47971
Leonardo Eloy <eloy@hp.com>
2016-08-25 21:27:06 -0600

    Displays number of opened pull requests

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+const {ipcMain} = require('electron');
 console.log('Loading Mutuca Pull Request');
 
-const version = '1.0.3';
+const version = '1.0.4';
 
 class MutucaPullRequest {
   constructor() {}
 
   pluginName() {
     return "mutuca-pull-request";
+  }
+
+  pluginType() {
+    return "card";
   }
 
   cardSize() {
@@ -25,5 +30,21 @@ class MutucaPullRequest {
     return this.pluginName() + "@" + version;
   }
 }
+
+ipcMain.on('plugin-tick', (event) => {
+  let mutucaApp = global.app.mutucaApp;
+  mutucaApp.github.pullRequests.getAll({
+    repo: mutucaApp.config.get("context").github.repository,
+    user: mutucaApp.config.get("context").github.owner,
+    state: "open"
+  }, (error, response) => {
+    if (error) {
+      console.log('error: ' + error);
+      return;
+    }
+
+    mutucaApp.window.send('mutuca-pull-request-update', response);
+  });
+});
 
 module.exports = MutucaPullRequest;
